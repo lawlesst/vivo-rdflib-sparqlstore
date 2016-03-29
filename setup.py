da32f549ef882680484a20084d5cfa5f7106f5e7
Ted Lawless <lawlesst@gmail.com>
2016-03-29 10:07:08 -0400

    Read requirements. Add vivoUpdate to modules.

--- setup.py
+++ setup.py
@@ -2,19 +2,19 @@ from setuptools import setup
 
 description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.'
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setup(
     name = 'vivo-rdflib-sparqlstore',
     version = '0.0.1',
     url = 'https://github.com/lawlesst/rdflib-vivo-sparqlstore',
     author = 'Ted Lawless',
     author_email = 'lawlesst@gmail.com',
-    py_modules = ['vstore',],
+    py_modules = ['vstore', 'vivoUpdate'],
     scripts = ['vstore.py'],
     description = description,
-    install_requires=[
-        'rdflib>=4.2',
-        'click'
-    ],
+    install_requires=required,
     entry_points='''
         [console_scripts]
         vivoUpdate=vivoUpdate:process
