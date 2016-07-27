59d3c49e49376e53b5c6bea2cadfd1d6140474a6
Ted Lawless <lawlesst@gmail.com>
2016-07-27 15:05:56 -0400

    Specify requirements.

--- setup.py
+++ setup.py
@@ -2,17 +2,20 @@ from setuptools import setup, find_packages
 
 description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.'
 
-with open('requirements.txt') as f:
-    required = f.read().splitlines()
+# with open('requirements.txt') as f:
+#     required = f.read().splitlines()
 
 setup(
     name = 'vivo-rdflib-sparqlstore',
-    version = '0.0.1',
+    version = '0.1',
     url = 'https://github.com/lawlesst/rdflib-vivo-sparqlstore',
     author = 'Ted Lawless',
     author_email = 'lawlesst@gmail.com',
     packages=find_packages(),
-    install_requires=required,
+    install_requires=[
+        'rdflib>=4',
+        'click'
+    ],
     include_package_data=True,
     description = description,
     entry_points='''
