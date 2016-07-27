a7b2d1a0a34f40a23af3a7f81c741f8533941f0c
Ted Lawless <lawlesst@gmail.com>
2016-07-27 15:07:21 -0400

    Up version.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.
 
 setup(
     name = 'vivo-rdflib-sparqlstore',
-    version = '0.1',
+    version = '0.11',
     url = 'https://github.com/lawlesst/rdflib-vivo-sparqlstore',
     author = 'Ted Lawless',
     author_email = 'lawlesst@gmail.com',
