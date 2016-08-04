205c381c58b33450273a39c881f3eb20659e4fa1
Ted Lawless <lawlesst@gmail.com>
2016-08-04 16:32:41 -0400

    Bump version. Formatting.

--- setup.py
+++ setup.py
@@ -2,22 +2,19 @@ from setuptools import setup, find_packages
 
 description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.'
 
-# with open('requirements.txt') as f:
-#     required = f.read().splitlines()
-
 setup(
-    name = 'vivo-rdflib-sparqlstore',
-    version = '0.11',
-    url = 'https://github.com/lawlesst/rdflib-vivo-sparqlstore',
-    author = 'Ted Lawless',
-    author_email = 'lawlesst@gmail.com',
+    name='vivo-rdflib-sparqlstore',
+    version='0.2',
+    url='https://github.com/lawlesst/rdflib-vivo-sparqlstore',
+    author='Ted Lawless',
+    author_email='lawlesst@gmail.com',
     packages=find_packages(),
     install_requires=[
         'rdflib>=4',
         'click'
     ],
     include_package_data=True,
-    description = description,
+    description=description,
     entry_points='''
         [console_scripts]
         vivoUpdate=scripts.command:process
