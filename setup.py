f1561ce8a3f20e38c66ffb825fc0a6bf27514286
Ted Lawless <lawlesst@gmail.com>
2018-05-28 08:52:39 -0400

    Bump version. Change dependencies.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ description = 'RDFLib SPARQLStore and SPARQLUpdateStore implementation for VIVO.
 
 setup(
     name='vivo-rdflib-sparqlstore',
-    version='0.2',
+    version='0.3',
     url='https://github.com/lawlesst/rdflib-vivo-sparqlstore',
     author='Ted Lawless',
     author_email='lawlesst@gmail.com',
     packages=find_packages(),
     install_requires=[
-        'rdflib==4.2',
-        'SPARQLWrapper==1.8',
-        'click==5.0'
+        'rdflib>=4.2',
+        'SPARQLWrapper>=1.8',
+        'click>=5.0'
     ],
     include_package_data=True,
     description=description,
